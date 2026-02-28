ccfea15419322ab20a7825b2dff413d3f7f3b90f
Phobos <case_scale@protonmail.com>
2026-02-28 16:55:36 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,13 +8,19 @@ in
 
   # Enable networking
   networking.networkmanager.enable = true;
-  
+
+  # Hardware
+  hardware = {
+    enableAllHardware = true; 
+    enableRedistributableFirmware = true;
+  };
+
   # ISO overrides
   image.modules.iso = {
     isoImage = {
-      squashfsCompression = "zstd -Xcompression-level 6";
+      #squashfsCompression = "zstd -Xcompression-level 6";
       # Smaller build, longer build time.
-      #squashfsCompression = "xz -Xdict-size 100% -b 1M"; 
+      squashfsCompression = "xz -Xdict-size 100% -b 1M"; 
       makeEfiBootable = lib.mkForce true;
     };
     # faster
@@ -23,7 +29,7 @@ in
     #isoImage.squashfsCompression = "lz4";
   };
 
-  boot.loader.timeout = lib.mkForce 1;
+  boot.loader.timeout = lib.mkForce 0;
 
   # xfce
   services = {
@@ -42,6 +48,9 @@ in
       user = user;
     };
   };
+
+  #Auto-mounting USB/MTP for Android
+  services.gvfs.enable = true;
   
   # Excluded xfce packages
   environment.xfce.excludePackages = with pkgs.xfce; [
@@ -81,8 +90,86 @@ in
 
   programs.adb.enable = true;
 
-  home-manager.users.mtkclient = { ... }: 
-    {
+  home-manager.users.mtkclient = { config, pkgs, lib, ... }: 
+    let
+      desktop-dir = config.xdg.userDirs.desktop;
+      desktop-rel = lib.strings.removePrefix (config.home.homeDirectory + "/") desktop-dir;
+    in {
+
+    home = {
+      username = user;
+      homeDirectory = "/home/${user}";
+      stateVersion = "25.11";
+      file = {
+        "Desktop/Garaho Wiki.desktop" = {
+          executable = true;
+          text = ''
+            [Desktop Entry]
+            Type=Application
+            Name=Garaho Wiki
+            Exec=librewolf https://garahowiki.com/
+            Icon=applications-internet
+            Terminal=false
+          '';
+          };
+      
+        # Browser
+        ".config/xfce4/panel/launcher-13/browser.desktop" = {
+          executable = true;
+          text = ''
+            [Desktop Entry]
+            Type=Application
+            Name=Web
+            Exec=librewolf
+            Icon=web-browser
+            Terminal=false
+          '';
+          };
+
+        # File Manager
+        ".config/xfce4/panel/launcher-14/files.desktop" = {
+          executable = true;
+          text = ''
+            [Desktop Entry]
+            Type=Application
+            Name=Files
+            Exec=thunar
+            Icon=system-file-manager
+            Terminal=false
+          '';
+          };
+
+          # Terminal
+        ".config/xfce4/panel/launcher-15/terminal.desktop" = {
+          executable = true;
+          text = ''
+            [Desktop Entry]
+            Type=Application
+            Name=Terminal
+            Exec=xfce4-terminal
+            Icon=utilities-terminal
+            Terminal=false
+          '';
+          };
+      
+      
+        ".config/xfce4/panel/launcher-16/xfce4-appfinder.desktop" = {
+          executable = true;
+          text = ''
+            [Desktop Entry]
+            Version=1.0
+            Type=Application
+            Name=Application Finder
+            Comment=Find and launch applications
+            Exec=xfce4-appfinder
+            Icon=org.xfce.appfinder
+            Terminal=false
+            Categories=Utility;X-XFCE;
+            StartupNotify=true
+          '';
+          };
+    };
+
     xdg = {
       configFile."xfce4/helpers.rc".text = ''
       WebBrowser=librewolf
@@ -115,10 +202,108 @@ in
           "desktop-icons/file-icons/show-home"       = false;
           "desktop-icons/file-icons/show-filesystem" = false;
         };
+        xfce4-panel = {
+          
+          "configver" = 2;
+
+          "panels" = [ 1 2 ];
+
+          # Dark-mode
+          "panels/dark-mode" = true;
+
+          # Panel 1
+          "panels/panel-1/icon-size" = 16;
+          "panels/panel-1/length" = 100;
+          "panels/panel-1/plugin-ids" = [ 1 2 3 4 5 6 7 8 9 10 19 ];
+          "panels/panel-1/position" = "p=6;x=0;y=0";
+          "panels/panel-1/position-locked" = true;
+          "panels/panel-1/size" = 26;
+          
+          # Panel 2
+          "panels/panel-2/autohide-behavior" = 1;
+          "panels/panel-2/length" = 1;
+          "panels/panel-2/plugin-ids" = [ 11 12 13 14 15 16 17 18 ];
+          "panels/panel-2/position" = "p=10;x=0;y=0";
+          "panels/panel-2/position-locked" = true;
+          "panels/panel-2/size" = 48;
+
+          # Panel 1 Plugins
+          "plugins/plugin-1" = "applicationsmenu";
+
+          "plugins/plugin-2" = "tasklist";
+          "plugins/plugin-2/grouping" = 1;
+
+          "plugins/plugin-3" = "separator";
+          "plugins/plugin-3/expand" = true;
+          "plugins/plugin-3/style" = 0;
+
+          "plugins/plugin-4" = "pager";
+
+          "plugins/plugin-5" = "separator";
+          "plugins/plugin-5/style" = 0;
+
+          "plugins/plugin-6" = "systray";
+
+          "plugins/plugin-6/known-legacy-items" = [
+            "ethernet network connection \"wired connection 1\" active"
+          ];
+          "plugins/plugin-6/square-icons" = true;
+
+          "plugins/plugin-7" = "separator";
+          "plugins/plugin-7/style" = 0;
+
+          "plugins/plugin-8" = "clock";
+
+          "plugins/plugin-9" = "separator";
+          "plugins/plugin-9/style" = 0;
+
+          "plugins/plugin-10" = "actions";
+          "plugins/plugin-10/appearance" = 0;
+          "plugins/plugin-10/items" = [
+            "-lock-screen"
+            "-switch-user"
+            "+separator"
+            "+suspend"
+            "-hibernate"
+            "-hybrid-sleep"
+            "-separator"
+            "+restart"
+            "+shutdown"
+            "+separator"
+            "-logout"
+            "-logout-dialog"
+          ];
+
+          # Panel 2 Config
+          "plugins/plugin-11" = "showdesktop";
+
+          "plugins/plugin-12" = "separator";
+
+          "plugins/plugin-13" = "launcher";
+          "plugins/plugin-13/items" = [ "browser.desktop" ];
+
+          "plugins/plugin-14" = "launcher";
+          "plugins/plugin-14/items" = [ "files.desktop" ];
+
+          "plugins/plugin-15" = "launcher";
+          "plugins/plugin-15/items" = [ "terminal.desktop" ];
+
+          "plugins/plugin-16" = "launcher";
+          "plugins/plugin-16/items" = [ "xfce4-appfinder.desktop" ];
+
+          "plugins/plugin-17" = "separator";
+
+          "plugins/plugin-18" = "directorymenu";
+          "plugins/plugin-18/base-directory" = "/home/${user}";
+          
+          "plugins/plugin-19" = "power-manager-plugin";
+
+        };
       };
     };
   };
 
+  # Remove printing services
   services.system-config-printer.enable = false;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
